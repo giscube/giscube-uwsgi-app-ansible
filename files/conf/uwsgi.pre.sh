a1c9da4c0d51ddebb3cc8efebe275668400a95f9
Manel Clos <manelclos@gmail.com>
2017-08-04 18:05:15 +0200

    remove previous uwsgi.sock if it exists, create link when deploying

--- files/conf/uwsgi.pre.sh
+++ files/conf/uwsgi.pre.sh
@@ -11,5 +11,8 @@ test -e $DIR/run || mkdir -p $DIR/run
 chown www-data:www-data $DIR/run
 chmod 770 $DIR/run
 
+# check if socket file exists and it is not a link
+test -e $DIR/uwsgi.sock && (test -L $DIR/uwsgi.sock || rm -f $DIR/uwsgi.sock)
+
 # check if socket link exists, create if not
 test -L $DIR/uwsgi.sock || ln -sr $DIR/run/uwsgi.sock $DIR/uwsgi.sock
